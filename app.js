59256596952e6518b50bc19df5ae7ec0771c9db2
Anahit <anahit.sargsyana@ingoarmenia.am>
2025-07-05 00:15:45 +0400

    Setup node project with express

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import healthRoutes from "./routes/healthRoutes.js";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", healthRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
